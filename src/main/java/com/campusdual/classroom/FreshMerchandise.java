517aac0a774b9246f74ccc4c6636765af5c2fd78
Ana Martínez Puga <anammpuga@gmail.com>
2024-10-01 09:39:30 +0200

    Lo he hecho pero el test me da un error que no entiendo

--- src/main/java/com/campusdual/classroom/FreshMerchandise.java
+++ src/main/java/com/campusdual/classroom/FreshMerchandise.java
@@ -5,4 +5,26 @@ import java.util.Date;
 
 public class FreshMerchandise extends Merchandise {
 
+    Date expirationDate;
+
+    public FreshMerchandise(String name, String uniqueId, String responsibleId, int zone, String area, String shelf, int quantity, Date expirationDate) {
+        super(name, uniqueId, responsibleId, zone, area, shelf, quantity);
+        this.expirationDate = expirationDate;
+    }
+
+    public String getSpecificData() {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        String fecha ="La localización es: " + getLocation() + ". La fecha de caducidad es: "+ dateFormat.format(expirationDate);
+        System.out.println(fecha);
+        return fecha;
+
+    }
+
+
+
+    public void printSpecificData() {
+        System.out.println(getSpecificData());
+
+    }
 }
